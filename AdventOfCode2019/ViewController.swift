c0c8feefe0657b69811731460f4dfc85593f7164
Geir-Kåre S. Wærp <geir-kare.waerp@shortcut.no>
2019-11-30 18:20:39 +0100

    Initial Commit

---
+++ AdventOfCode2019/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdventOfCode2019
+//
+//  Created by Geir-Kåre S. Wærp on 30/11/2019.
+//  Copyright © 2019 GK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
