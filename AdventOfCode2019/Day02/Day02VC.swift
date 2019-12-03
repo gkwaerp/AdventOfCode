9bca21f8f18e1948e7f1df5a8cec2a4e60ac3280
Geir-Kåre S. Wærp <geir-kare.waerp@shortcut.no>
2019-12-03 19:24:45 +0100

    Refactored IntMachine into its own thing.

--- AdventOfCode2019/Day02/Day02VC.swift
+++ AdventOfCode2019/Day02/Day02VC.swift
@@ -9,86 +9,12 @@
 import UIKit
 
 class Day02VC: AoCVC, AdventDay {
-    class OpcodeMachine {
-        enum Opcode {
-            case addition(indexA: Int, indexB: Int, resultingIndex: Int)
-            case multiplication(indexA: Int, indexB: Int, resultingIndex: Int)
-            case halt
-
-            static func from(ints: [Int], at offset: Int) -> Opcode {
-                switch ints[offset] {
-                case 1: return .addition(indexA: ints[offset + 1], indexB: ints[offset + 2], resultingIndex: ints[offset + 3])
-                case 2: return .multiplication(indexA: ints[offset + 1], indexB: ints[offset + 2], resultingIndex: ints[offset + 3])
-                case 99: return .halt
-                default: fatalError()
-                }
-            }
-        }
-
-        private var initialMemory = [Int]()
-        private var workingMemory = [Int]()
-        private var instructionPointer = 0
-
-        init(memory: [Int], noun: Int?, verb: Int?) {
-            self.loadNewProgram(memory: memory, noun: noun, verb: verb)
-        }
-
-        func loadNewProgram(memory: [Int], noun: Int?, verb: Int?) {
-            self.initialMemory = memory
-            self.reset(noun: noun, verb: verb)
-        }
-
-        func reset(noun: Int?, verb: Int?) {
-            self.workingMemory = self.initialMemory
-            self.instructionPointer = 0
-
-            if let noun = noun {
-                guard self.workingMemory.count > 1 else { fatalError() }
-                self.workingMemory[1] = noun
-            }
-            if let verb = verb {
-                guard self.workingMemory.count > 2 else { fatalError() }
-                self.workingMemory[2] = verb
-            }
-        }
-
-        private func incrementInstructionPointer(from opcode: Opcode) {
-            switch opcode {
-            case .addition: self.instructionPointer += 4
-            case .multiplication: self.instructionPointer += 4
-            case .halt: self.instructionPointer += 1
-            }
-        }
-
-        func runProgram() -> Int {
-            var finished = false
-            while !finished {
-                let opcode = Opcode.from(ints: self.workingMemory, at: self.instructionPointer)
-                finished = self.execute(opcode: opcode)
-                self.incrementInstructionPointer(from: opcode)
-            }
-            return self.workingMemory[0]
-        }
-
-        private func execute(opcode: Opcode) -> Bool {
-            switch opcode {
-                case .addition(let indexA, let indexB, let resultingIndex):
-                    self.workingMemory[resultingIndex] = self.workingMemory[indexA] + self.workingMemory[indexB]
-                case .multiplication(let indexA, let indexB, let resultingIndex):
-                    self.workingMemory[resultingIndex] = self.workingMemory[indexA] * self.workingMemory[indexB]
-                case .halt:
-                    return true
-            }
-            return false
-        }
-    }
-
-    private var machine: OpcodeMachine!
+    private var machine = IntMachine()
 
     func loadInput() {
         let line = FileLoader.loadText(fileName: "Day02Input").first!
         let ints = line.components(separatedBy: ",").compactMap({Int($0)})
-        self.machine = OpcodeMachine(memory: ints, noun: nil, verb: nil)
+        self.machine.loadNewProgram(memory: ints, noun: nil, verb: nil)
     }
 
     func solveFirst() {
---
+++ AdventOfCode2019/Helpers/IntMachine.swift
@@ -0,0 +1,85 @@
+//
+//  IntMachine.swift
+//  AdventOfCode2019
+//
+//  Created by Geir-Kåre S. Wærp on 03/12/2019.
+//  Copyright © 2019 GK. All rights reserved.
+//
+
+import Foundation
+
+class IntMachine {
+    enum Opcode {
+        case addition(indexA: Int, indexB: Int, resultingIndex: Int)
+        case multiplication(indexA: Int, indexB: Int, resultingIndex: Int)
+        case halt
+
+        static func from(ints: [Int], at offset: Int) -> Opcode {
+            switch ints[offset] {
+            case 1: return .addition(indexA: ints[offset + 1], indexB: ints[offset + 2], resultingIndex: ints[offset + 3])
+            case 2: return .multiplication(indexA: ints[offset + 1], indexB: ints[offset + 2], resultingIndex: ints[offset + 3])
+            case 99: return .halt
+            default: fatalError()
+            }
+        }
+    }
+
+    private var initialMemory = [Int]()
+    private var workingMemory = [Int]()
+    private var instructionPointer = 0
+
+    init() { }
+
+    init(memory: [Int], noun: Int?, verb: Int?) {
+        self.loadNewProgram(memory: memory, noun: noun, verb: verb)
+    }
+
+    func loadNewProgram(memory: [Int], noun: Int?, verb: Int?) {
+        self.initialMemory = memory
+        self.reset(noun: noun, verb: verb)
+    }
+
+    func reset(noun: Int?, verb: Int?) {
+        self.workingMemory = self.initialMemory
+        self.instructionPointer = 0
+
+        if let noun = noun {
+            guard self.workingMemory.count > 1 else { fatalError() }
+            self.workingMemory[1] = noun
+        }
+        if let verb = verb {
+            guard self.workingMemory.count > 2 else { fatalError() }
+            self.workingMemory[2] = verb
+        }
+    }
+
+    private func incrementInstructionPointer(from opcode: Opcode) {
+        switch opcode {
+        case .addition: self.instructionPointer += 4
+        case .multiplication: self.instructionPointer += 4
+        case .halt: self.instructionPointer += 1
+        }
+    }
+
+    func runProgram() -> Int {
+        var finished = false
+        while !finished {
+            let opcode = Opcode.from(ints: self.workingMemory, at: self.instructionPointer)
+            finished = self.execute(opcode: opcode)
+            self.incrementInstructionPointer(from: opcode)
+        }
+        return self.workingMemory[0]
+    }
+
+    private func execute(opcode: Opcode) -> Bool {
+        switch opcode {
+            case .addition(let indexA, let indexB, let resultingIndex):
+                self.workingMemory[resultingIndex] = self.workingMemory[indexA] + self.workingMemory[indexB]
+            case .multiplication(let indexA, let indexB, let resultingIndex):
+                self.workingMemory[resultingIndex] = self.workingMemory[indexA] * self.workingMemory[indexB]
+            case .halt:
+                return true
+        }
+        return false
+    }
+}
