a9d4180330f02184ed7c81f7de8e1a245669321a
Geir-Kåre S. Wærp <geir-kare.waerp@shortcut.no>
2019-12-04 17:40:07 +0100

    Renaming things.

--- AdventOfCode2019/Day03/Day03VC.swift
+++ AdventOfCode2019/Day03/Day03VC.swift
@@ -25,20 +25,20 @@ class Day03VC: AoCVC, AdventDay {
             let components = string.components(separatedBy: ",")
             var currPoint = IntPoint(x: 0, y: 0)
             for component in components {
-                let direction = component.first!
+                let directionString = component.first!
                 let numSteps = Int(component.dropFirst())!
                 
-                var directionalPoint: IntPoint!
-                switch direction {
-                case "U": directionalPoint = IntPoint(x: 0, y: 1)
-                case "D": directionalPoint = IntPoint(x: 0, y: -1)
-                case "L": directionalPoint = IntPoint(x: -1, y: 0)
-                case "R": directionalPoint = IntPoint(x: 1, y: 0)
+                var direction: IntPoint!
+                switch directionString {
+                case "U": direction = IntPoint(x: 0, y: 1)
+                case "D": direction = IntPoint(x: 0, y: -1)
+                case "L": direction = IntPoint(x: -1, y: 0)
+                case "R": direction = IntPoint(x: 1, y: 0)
                 default: fatalError()
                 }
                 
                 for _ in 0..<numSteps {
-                    currPoint = IntPoint.adding(currPoint, directionalPoint)
+                    currPoint = IntPoint.adding(currPoint, direction)
                     self.addPoint(currPoint)
                 }
             }
@@ -61,7 +61,7 @@ class Day03VC: AoCVC, AdventDay {
     
     func solveFirst() {
         let intersections = self.wirePaths[0].intersections(with: self.wirePaths[1])
-        let closest = intersections.map({$0.distance(to: .origin)}).sorted().first!
+        let closest = intersections.map({$0.manhattanDistance(to: .origin)}).sorted().first!
         self.setSolution1("\(closest)")
     }
     
--- AdventOfCode2019/Helpers/IntPoint.swift
+++ AdventOfCode2019/Helpers/IntPoint.swift
@@ -12,12 +12,16 @@ struct IntPoint: Hashable {
     let x: Int
     let y: Int
     
-    func distance(to other: IntPoint = .origin) -> Int {
+    func manhattanDistance(to other: IntPoint = .origin) -> Int {
         return abs(self.x - other.x) + abs(self.y - other.y)
     }
     
     static func adding(_ a: IntPoint, _ b: IntPoint) -> IntPoint {
         return IntPoint(x: a.x + b.x, y: a.y + b.y)
+    }
+
+    static func subtracting(_ a: IntPoint, _ b: IntPoint) -> IntPoint {
+        return IntPoint(x: a.x - b.x, y: a.y - b.y)
     }
     
     static var origin: IntPoint {
